8c1c7c243dfd1562b42c75e7cc6ca412dd2cea95
Andres Palacios <andrespvzla@gmail.com>
2021-06-08 17:06:52 -0400

    Another try, common

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/moviesapp'));
-app.get('/', function(req,res) {
+app.use(express.static(__dirname + '/dist/moviesApp'));
+app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/moviesapp/index.html'));});
+'/dist/moviesApp/index.html'));});
 app.listen(process.env.PORT || 8080);
